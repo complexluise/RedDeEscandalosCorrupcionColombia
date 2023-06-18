656c5e02f2fb79f4bcbda207e66b7a007fc2fbbd
Luis Higuera <lhiguerac@unal.edu.co>
2023-06-18 05:09:23 -0500

    return json

--- get_txt_from_news.py
+++ get_txt_from_news.py
@@ -1,8 +1,9 @@
-from utils.any_news_scrapper import NewsScraper, ParserNews
-from utils.sheets import GoogleSheet
+import json
 import numpy as np
 import logging
 
+from utils.any_news_scrapper import NewsScraper, ParserNews
+from utils.sheets import GoogleSheet
 # Create logger
 LOG = logging.getLogger("NewsScraper")
 logging.basicConfig(level=logging.INFO)
@@ -28,15 +29,20 @@ df_links_to_process_raw = df_links_raw[~bool_mask]
 df_links_to_process_filtered = df_links_to_process_raw.replace('', np.nan)
 
 
-df_links_to_process = df_links_to_process_filtered.dropna(subset=['Link', 'Nombre Entidad', 'Escandalo'])
+df_links_to_process = df_links_to_process_filtered.dropna(subset=['Fuente', 'Escandalo', 'Link', 'Titulo', 'Autor'])
 
 for index, row in df_links_to_process.iterrows():
     
+    item = {
+        "Titulo": row['Titulo'],
+        "Autor": row['Autor'],
+        "URL": row['Link'],
+        "Contenido": raw_text,
+        "EscandaloDeCorrupcion": row['Escandalo'],
+        "Fuente": row['Fuente'],
+    }
     
-    url = row['Link']
-    pagina = row['Nombre Entidad']
-    escandalo = row['Escandalo']
-    filename = url.split("/")[-1][:100]
+    filename = item['Titulo'][:100]
     LOG.info("Processing row: %s", filename)
     
     pages_indicator = {
@@ -45,22 +51,31 @@ for index, row in df_links_to_process.iterrows():
         "eltiempo": {"html_tag": "div", "indicator": {"class": "articulo-contenido"}},
         "wikipedia": {"html_tag": "div", "indicator": {"class": "mw-parser-output"}},
         "RT": {"html_tag": "div", "indicator": {"class": "article__text"}},
+        # semana
+        # las2orillas
+        # elnuevosiglo
+        # Caracol
+        # RTVC
     }
     
-    indicator = pages_indicator.get(pagina, None)
+    indicator = pages_indicator.get(item['Fuente'], None)
     
     # Si no no existe la pagina debe continuar con el siguiente
     if indicator is None:
-        LOG.warning("Page not found: %s", pagina)
+        LOG.warning("Page not found: %s", item['Fuente'])
         continue
     try:
-        response = NewsScraper(url).get_news() # TODO Iniciar fuera del for
+        response = NewsScraper(item['URL']).get_news() # TODO Iniciar fuera del for
         parser = ParserNews(response)
         raw_text = parser.parse_page(indicator)
+        
+
+        
         # save file to txt
-        with open("data/raw_article/"+pagina+filename+'.txt', "w", encoding="utf-8") as file:
-            LOG.info("Saving file: %s", filename)
-            file.write(raw_text)
+        with open("data/raw_article/"+item['Fuente']+"_"+item['Titulo'][:20]+'.json', "w", encoding="utf-8") as file:
+            LOG.info("Saving file: %s", item['Titulo'])
+            file.write(json.dumps(item, indent=4, ensure_ascii=False))
+                       
     except Exception as e:
         LOG.error("Error processing row: %s", filename)
         LOG.error(e)
