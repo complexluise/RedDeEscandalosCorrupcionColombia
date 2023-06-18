e676d23f818da347f6c40484be9e08523c040559
Luis Higuera <lhiguerac@unal.edu.co>
2023-06-18 13:35:30 -0500

    :sparkles: add put data sheets

---
+++ put_data_sheets.py
@@ -0,0 +1,82 @@
+import os
+import json
+import pandas as pd
+from utils.sheets import GoogleSheet
+
+import logging
+
+# Create logger
+LOG = logging.getLogger("Put Data Sheets")
+logging.basicConfig(level=logging.INFO)
+
+LOG.info("Starting Extract Scandal Corruption Network")
+
+SPREADSHEET_ID = '1lnrxTIbBYYrMwodD7cEmolC7BQk1NVVrFjQiQon8tzQ'
+# Initialize Google Sheet
+gsheet = GoogleSheet(SPREADSHEET_ID)
+
+
+# iterate by filenames in data/processed_article
+# lista los archivos crudos en la carpeta data/raw_article
+list_files = os.listdir("data/processed_article")
+# Filtra solo los archivos JSON
+list_files = [file for file in list_files if file.endswith(".json")]
+
+
+# itera sobre los archivos
+
+for filename in list_files:
+
+    LOG.info("Processing file: %s", filename)
+    
+    with open(f'data/processed_article/{filename}', "r", encoding="utf-8") as file:
+        json_news = json.loads(file.read())
+
+    with open("data/extracted_entities/" + filename, "r", encoding="utf-8") as file:
+        json_NER = json.loads(file.read())
+
+    with open("data/extracted_relations/" + filename, "r", encoding="utf-8") as file:
+        json_ER = json.loads(file.read())
+        
+
+    escandalo = json_news['EscandaloDeCorrupcion']
+    noticia_url = json_news['URL']
+
+    # Save to Google Sheet Noticia
+    df_noticia = pd.DataFrame([json_news])
+    gsheet.add_csv_to_sheet(df_noticia, 'Noticia')
+
+    # Save to Google Sheet Entities
+    ## Individuo
+    df_individuo = pd.DataFrame(json_NER['Individuos'])
+    df_individuo['Escandalo'] = escandalo
+    df_individuo['Noticia'] = noticia_url
+    gsheet.add_csv_to_sheet(df_individuo, 'Individuo')
+
+    ## Organización
+    df_organizacion = pd.DataFrame(json_NER['Organizaciones'])
+    df_organizacion['Escandalo'] = escandalo
+    df_organizacion['Noticia'] = noticia_url
+    gsheet.add_csv_to_sheet(df_organizacion, 'Organizacion')
+
+    # Save to Google Sheet Relaciones
+    # Append a la hoja Relaciones
+    df_rel_ind_org = pd.DataFrame(json_ER['Relaciones'])
+    df_rel_ind_org['Escandalo'] = escandalo
+    df_rel_ind_org['Noticia'] = noticia_url
+    gsheet.add_csv_to_sheet(df_rel_ind_org, 'Relaciones Ind-Org')
+
+    # Relación Individuo Individuo
+    df_rel_ind_ind = pd.DataFrame(json_ER['Relaciones'])
+    df_rel_ind_ind['Escandalo'] = escandalo
+    df_rel_ind_ind['Noticia'] = noticia_url
+    gsheet.add_csv_to_sheet(df_rel_ind_ind, 'Relaciones Ind-Ind')
+
+    # at the end move file to data/processed_article/escandalo_name
+    # move file to data/processed_article/escandalo_name
+    os.makedirs(f'data/processed_article/{escandalo}', exist_ok=True)
+    with open("data/processed_article/" + f'{escandalo}/'+ filename, "w", encoding="utf-8") as file:
+        file.write(json.dumps(json_news))
+
+    # delete file from raw_article
+    os.remove(f'data/processed_article/{filename}')
