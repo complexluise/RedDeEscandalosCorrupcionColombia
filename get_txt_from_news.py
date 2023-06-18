a071fbfe7f81917296568748fd143cafe6297f88
Luis Higuera <lhiguerac@unal.edu.co>
2023-06-18 11:02:10 -0500

    add new indicators for some sources

--- get_txt_from_news.py
+++ get_txt_from_news.py
@@ -9,7 +9,7 @@ LOG = logging.getLogger("NewsScraper")
 logging.basicConfig(level=logging.INFO)
 
 SPREADSHEET_ID = '1jc6qII2_ERr1mOE66WPT0vntL1xc7pT4Jupo3ef552k' # Links de Noticias
-RANGE_NAME = 'Links!A1:E1000'
+RANGE_NAME = 'Links!A1:G1000'
 
 LOG.info("Starting NewsScraper")
 
@@ -29,20 +29,19 @@ df_links_to_process_raw = df_links_raw[~bool_mask]
 df_links_to_process_filtered = df_links_to_process_raw.replace('', np.nan)
 
 
-df_links_to_process = df_links_to_process_filtered.dropna(subset=['Fuente', 'Escandalo', 'Link', 'Titulo', 'Autor'])
+df_links_to_process = df_links_to_process_filtered.dropna(subset=['Fuente', 'Escandalo', 'Link', 'Titulo'])
 
 for index, row in df_links_to_process.iterrows():
     
     item = {
         "Titulo": row['Titulo'],
-        "Autor": row['Autor'],
+        "Autor": '' if row['Autor'] is np.nan else row['Autor'],
         "URL": row['Link'],
-        "Contenido": raw_text,
         "EscandaloDeCorrupcion": row['Escandalo'],
         "Fuente": row['Fuente'],
     }
     
-    filename = item['Titulo'][:100]
+    filename = item['Titulo'][:100].replace(":", "_")
     LOG.info("Processing row: %s", filename)
     
     pages_indicator = {
@@ -51,7 +50,10 @@ for index, row in df_links_to_process.iterrows():
         "eltiempo": {"html_tag": "div", "indicator": {"class": "articulo-contenido"}},
         "wikipedia": {"html_tag": "div", "indicator": {"class": "mw-parser-output"}},
         "RT": {"html_tag": "div", "indicator": {"class": "article__text"}},
-        # semana
+        "semana": {"html_tag": "div", "indicator": {"class": "article-main"}},
+        "elespectador": {"html_tag": "article"},
+        "portafolio": {"html_tag": "div", "indicator": {"class": "article-content"}},
+        "elpais": {"html_tag": "div", "indicator": {"class": "paywall", "data-index": "paywall"}},
         # las2orillas
         # elnuevosiglo
         # Caracol
@@ -68,11 +70,10 @@ for index, row in df_links_to_process.iterrows():
         response = NewsScraper(item['URL']).get_news() # TODO Iniciar fuera del for
         parser = ParserNews(response)
         raw_text = parser.parse_page(indicator)
-        
-
+        item.update({"content": raw_text})
         
         # save file to txt
-        with open("data/raw_article/"+item['Fuente']+"_"+item['Titulo'][:20]+'.json', "w", encoding="utf-8") as file:
+        with open("data/raw_article/"+item['Fuente']+"_"+filename+'.json', "w", encoding="utf-8") as file:
             LOG.info("Saving file: %s", item['Titulo'])
             file.write(json.dumps(item, indent=4, ensure_ascii=False))
                        
